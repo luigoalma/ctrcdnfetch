a238793529cfa8dd61a6ceda41b43000c533ac11
luigoalma <luigoalma@hotmail.com>
2019-09-16 22:22:57 +0100

    ALIGN

--- types.h
+++ types.h
@@ -1,5 +1,4 @@
-#ifndef __TYPES_H__
-#define __TYPES_H__
+#pragma once
 #include <inttypes.h>
 
 typedef uint8_t u8;
@@ -13,4 +12,8 @@ typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
 
+#ifdef _MSC_VER
+#define ALIGN(x) __declspec(align(x))
+#else
+#define ALIGN(x) __attribute__((aligned(x)))
 #endif
