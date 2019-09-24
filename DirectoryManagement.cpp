b91fb7e3b8f715874e9a9e69e462ba6add3c800f
luigoalma <luigoalma@hotmail.com>
2019-09-25 00:46:26 +0100

    Remove directory code and path fixups
    Some minor things also
    Mainly untested

--- DirectoryManagement.cpp
+++ DirectoryManagement.cpp
@@ -6,18 +6,25 @@
 #include <cstdlib>
 #include <cstring>
 #include <string>
+#include <stdexcept>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <errno.h>
 #include <dirent.h>
+#include <unistd.h>
 #include "DirectoryManagement.hpp"
 
 #if defined _WIN16 || defined _WIN32 || defined _WIN64
+#ifndef __CYGWIN__
 #define goodmkdir(x,y) mkdir(x)
+#define lstat(x,y) stat(x,y)
+#endif
 #define DELIMITORS "/\\"
+#define DEFAULT_DELIMITOR '\\'
 #else
 #define goodmkdir(x,y) mkdir(x,y)
 #define DELIMITORS "/"
+#define DEFAULT_DELIMITOR '/'
 #endif
 
 int Utils::DirectoryManagement::MakeDirectory(const char* path, bool recursive) {
@@ -42,22 +49,22 @@ int Utils::DirectoryManagement::MakeDirectory(const char* path, bool recursive) 
 		char* workstr = copypath;
 		std::string progressivepath("");
 		do {
-			#if (defined _WIN16 || defined _WIN32 || defined _WIN64) && !defined __CYGWIN__
+			#if defined _WIN16 || defined _WIN32 || defined _WIN64
 			//if [A to Z]: path start 
 			if((path[0]&0xDF) >= 'A' && (path[0]&0xDF) <= 'Z' && path[1] == ':' && (path[2] == '\\' || path[2] == '/')) {
 				progressivepath += path[0];
-				progressivepath += ":/";
+				progressivepath += ":\\";
 				if(stat(progressivepath.c_str(), &buffer) != 0) return errno;
 				workstr = copypath + 2;
 				break;
 			}
 			if(path[0] == '\\') {
-				progressivepath += '/';
+				progressivepath += DEFAULT_DELIMITOR;
 				break;
 			}
 			#endif
 			if(path[0] == '/')
-				progressivepath += '/';
+				progressivepath += DEFAULT_DELIMITOR;
 		} while(0);
 		char* save = NULL, *str = workstr;
 		for(;; str = NULL) {
@@ -74,16 +81,16 @@ int Utils::DirectoryManagement::MakeDirectory(const char* path, bool recursive) 
 					break;
 				}
 			}
-			progressivepath += '/';
+			progressivepath += DEFAULT_DELIMITOR;
 		}
 		free(copypath);
 	} else if(errno == ENOENT) {
-		#if (defined _WIN16 || defined _WIN32 || defined _WIN64) && !defined __CYGWIN__
+		#if defined _WIN16 || defined _WIN32 || defined _WIN64
 		//if [A to Z]: path start 
 		if((path[0]&0xDF) >= 'A' && (path[0]&0xDF) <= 'Z' && path[1] == ':' && (path[2] == '\\' || path[2] == '/')) {
 			std::string checkpath("");
 			checkpath += path[0];
-			checkpath += ":/";
+			checkpath += ":\\";
 			if(stat(checkpath.c_str(), &buffer) != 0) return errno;
 		}
 		#endif
@@ -92,7 +99,74 @@ int Utils::DirectoryManagement::MakeDirectory(const char* path, bool recursive) 
 	return errno;
 }
 
-int Utils::DirectoryManagement::DirectoryListing(DirFileList& output, const char* path) noexcept {
+
+int Utils::DirectoryManagement::RemoveDirectory(const char* path, bool recursive) {
+	if(!path) {
+		errno = EFAULT;
+		return errno;
+	}
+	int ret = 0;
+	char* fixedpath = NULL;
+	char* tmp = NULL;
+	if((ret = FixUpPath(fixedpath, path, false)) != 0) return errno;
+	struct stat buffer;
+	if(lstat(fixedpath, &buffer) != 0) return errno;
+	if((buffer.st_mode&S_IFMT) != S_IFDIR) {
+		errno = ENOTDIR;
+		return errno;
+	}
+	if(!recursive) { //just try delete given path, as is
+		errno = 0;
+		rmdir(fixedpath);
+		ret = errno;
+		free(fixedpath);
+		errno = ret;
+		return ret;
+	}
+	//my attempt at not doing recursive calling
+	class helper {
+	public:
+		DirFileList path;
+		size_t index;
+		helper(const char* _path) : path(_path), index(0) {}
+		helper(const std::string& _path) : path(_path), index(0) {}
+		helper(helper&& other) {
+			path = std::move(other.path);
+			index = other.index;
+			other.index = 0;
+		}
+	};
+	try {
+		std::vector<helper> v;
+		v.push_back(helper(fixedpath));
+		free(fixedpath);
+		fixedpath = nullptr;
+		while(!v.empty()) {
+			while(v.back().index < v.back().path.size()) {
+				if(v.back().path[v.back().index].GetType() == 0) { //folder
+					auto tmp = v.back().index++;
+					v.push_back(helper(v.back().path + v.back().path[tmp]));
+					continue;
+				}
+				else unlink((v.back().path + v.back().path[v.back().index]).c_str()); //file or other
+				v.back().index++;
+			}
+			if(rmdir(v.back().path.GetPath())) ret = errno;
+			v.pop_back();
+		}
+	} catch (std::length_error &e) {
+		ret = ENOMEM;
+	} catch (std::bad_alloc &e) {
+		ret = ENOMEM;
+	} catch (...) {
+		ret = ECANCELED;
+	}
+	free(fixedpath); //only needed if got an exception before the free above on the try block
+	errno = ret;
+	return ret;
+}
+
+int Utils::DirectoryManagement::DirectoryListing(DirFileList& output, const char* path) {
 	if(!path) {
 		errno = EFAULT;
 		return errno;
@@ -101,7 +175,7 @@ int Utils::DirectoryManagement::DirectoryListing(DirFileList& output, const char
 	struct dirent *ep;
 	struct stat buffer;
 	memset(&buffer, 0, sizeof(struct stat));
-	errno = 0;
+	int ret = 0;
 	if(stat(path, &buffer) != 0) return errno;
 	if((buffer.st_mode&S_IFMT) != S_IFDIR) {
 		errno = ENOTDIR;
@@ -109,35 +183,34 @@ int Utils::DirectoryManagement::DirectoryListing(DirFileList& output, const char
 	}
 	if((dp = opendir(path)) == NULL) return errno;
 	output.Clear();
-	errno = 0; //ensure
 	while ((ep = readdir(dp))) {
 		if(!strcmp(ep->d_name, ".") || !strcmp(ep->d_name, "..")) continue;
 		DirFileList::entry* stuff = new(std::nothrow) DirFileList::entry();
 		if(!stuff) {
-			errno = ENOMEM;
+			ret = ENOMEM;
 			break;
 		}
 		size_t length = strlen(ep->d_name) + 1;
 		stuff->name = (char*)calloc(length, 1);
 		if(!stuff->name) {
 			delete stuff;
-			errno = ENOMEM;
+			ret = ENOMEM;
 			break;
 		}
 		strcpy(stuff->name, ep->d_name);
 		try {
 			std::string pathcheck(path);
-			#if (defined _WIN16 || defined _WIN32 || defined _WIN64) && !defined __CYGWIN__
+			#if defined _WIN16 || defined _WIN32 || defined _WIN64
 			if(pathcheck.back() != '\\' || pathcheck.back() != '/')
 			#else
 			if(pathcheck.back() != '/')
 			#endif
-				pathcheck += '/';
+				pathcheck += DEFAULT_DELIMITOR;
 			pathcheck += stuff->name;
-			if(stat(pathcheck.c_str(), &buffer) != 0) break;
+			if(lstat(pathcheck.c_str(), &buffer) != 0) break;
 		} catch (...) {
 			delete stuff;
-			errno = ENOMEM;
+			ret = ENOMEM;
 			break;
 		}
 		stuff->type = ((buffer.st_mode&S_IFMT) == S_IFDIR ? 0 : ((buffer.st_mode&S_IFMT) == S_IFREG ? 1 : 2));
@@ -145,16 +218,22 @@ int Utils::DirectoryManagement::DirectoryListing(DirFileList& output, const char
 			output.entries.push_back(stuff);
 		} catch (...) {
 			delete stuff;
-			errno = ENOMEM;
+			ret = ENOMEM;
 			break;
 		}
 	}
-	if(errno) output.Clear();
+	if(!ret) {
+		char* copy_path = NULL;
+		ret = FixUpPath(copy_path, path);
+		if(!ret) output.original_path = copy_path;
+	}
+	if(ret) output.Clear();
 	closedir(dp);
-	return errno;
+	errno = ret;
+	return ret;
 }
 
-int Utils::DirectoryManagement::CheckIfDir(const char* path) noexcept {
+int Utils::DirectoryManagement::CheckIfDir(const char* path) {
 	if(!path) {
 		errno = EFAULT;
 		return errno;
@@ -166,6 +245,57 @@ int Utils::DirectoryManagement::CheckIfDir(const char* path) noexcept {
 	if((buffer.st_mode&S_IFMT) != S_IFDIR)
 		errno = ENOTDIR;
 	return errno;
+}
+
+int Utils::DirectoryManagement::FixUpPath(char*& out, const char* path, bool ending_delimitor) {
+	if(!path) {
+		errno = EFAULT;
+		return errno;
+	}
+	std::string fixedpath("");
+	#if defined _WIN16 || defined _WIN32 || defined _WIN64
+	if(path[0] == '/') fixedpath += DEFAULT_DELIMITOR;
+	#else
+	if(path[0] == '/' || path[0] == '\\') fixedpath += DEFAULT_DELIMITOR;
+	#endif
+	char* copypath = (char*)malloc(strlen(path)+1);
+	if(!copypath) {
+		errno = ENOMEM;
+		return errno;
+	}
+	strcpy(copypath, path);
+	int ret = 0;
+	char* tmp = NULL;
+	try {
+		char* save = NULL, *str = copypath;
+		for(;; str = NULL) {
+			char* part = strtok_r(str, DELIMITORS, &save);
+			if(!part) break; //we finished.
+			fixedpath += part;
+			fixedpath += DEFAULT_DELIMITOR;
+		}
+		if(!ending_delimitor) fixedpath.pop_back();
+		tmp = (char*)calloc(fixedpath.size()+1,1);
+		if(!tmp) {
+			free(copypath);
+			errno = ENOMEM;
+			return errno;
+		}
+		fixedpath.copy(tmp, fixedpath.size());
+		out = tmp;
+	} catch (std::length_error &e) {
+		ret = ENOMEM;
+		free(tmp);
+	} catch (std::bad_alloc &e) {
+		ret = ENOMEM;
+		free(tmp);
+	} catch (...) {
+		ret = ECANCELED;
+		free(tmp);
+	}
+	free(copypath);
+	errno = ret;
+	return ret;
 }
 
 #undef goodmkdir
--- DirectoryManagement.hpp
+++ DirectoryManagement.hpp
@@ -4,47 +4,71 @@
 #include <stdexcept>
 #include <new>
 #include <vector>
+#include <string>
 
 namespace Utils {
 	namespace DirectoryManagement {
 		class DirFileList {
 		public:
 			class entry {
-			public:
+			private:
 				char* name;
 				int type; //0 folder, 1 file, 2 unknown or special types, -1 no entry.
-				entry& operator=(entry& other) {
-					if(this == &other) {
+			public:
+				const char* GetName() const {return name;}
+				int GetType() const {return type;}
+				entry& operator=(const entry& other) {
+					if(this != &other) {
 						char* tmpname = (char*)calloc(strlen(other.name)+1, 1);
 						if(!tmpname) throw std::bad_alloc();
 						free(name);
 						name = tmpname;
 						type = other.type;
+						strcpy(name, other.name);
+					}
+					return *this;
+				}
+				entry& operator=(entry&& other) {
+					if(this != &other) {
+						name = other.name;
+						other.name = nullptr;
+						type = other.type;
+						other.type = -1;
 					}
 					return *this;
 				}
 				entry() : name(NULL), type(-1) {}
-				entry(entry& other) : entry() {*this = other;}
+				entry(const entry& other) : entry() {*this = other;}
+				entry(entry&& other) : entry() {*this = other;}
 				~entry() {free(name);}
+				friend class DirFileList;
+				friend int DirectoryListing(DirFileList& output, const char* path);
 			};
 		private:
+			char* original_path;
 			std::vector<entry*> entries;
 			void Clear() {
 				for(size_t i = 0; i < entries.size(); i++)
 					delete entries[i];
 				entries.clear();
+				free(original_path);
+				original_path = nullptr;
 			}
 		public:
 			const entry& operator[](int index) const {
 				return *(entries.at(index));
 			}
-			DirFileList& operator=(DirFileList& other) {
+			DirFileList& operator=(const DirFileList& other) {
 				if(this != &other) {
 					Clear();
+					char* new_path = (char*)malloc(strlen(other.original_path)+1);
+					if(!new_path) throw std::bad_alloc();
+					strcpy(new_path, other.original_path);
+					original_path = new_path;
 					for(size_t i = 0; i < other.entries.size(); i++) {
 						entry* foo = nullptr;
 						try {
-							foo = new entry(*entries[i]);
+							foo = new entry(*other.entries[i]);
 							entries.push_back(nullptr);
 							entries[i] = foo;
 						} catch(...) {
@@ -56,15 +80,34 @@ namespace Utils {
 				}
 				return *this;
 			}
-			DirFileList() {}
-			DirFileList(DirFileList& other) {*this = other;}
+			DirFileList& operator=(DirFileList&& other) {
+				if(this != &other) {
+					free(original_path);
+					original_path = other.original_path;
+					other.original_path = nullptr;
+					entries = std::move(other.entries);
+				}
+				return *this;
+			}
+			std::string operator+(const entry& _entry) {
+				return std::string(std::string(original_path) + std::string(_entry.name));
+			}
+			size_t size() const {return entries.size();}
+			const char* GetPath() const {return original_path;};
+			DirFileList() : original_path(nullptr) {}
+			DirFileList(const char* path) : DirFileList() {DirectoryListing(*this, path);}
+			DirFileList(const std::string& path) : DirFileList() {DirectoryListing(*this, path.c_str());}
+			DirFileList(const DirFileList& other) : DirFileList() {*this = other;}
+			DirFileList(DirFileList&& other) : DirFileList() {*this = other;}
 			~DirFileList() {Clear();}
-			friend int DirectoryListing(DirFileList& output, const char* path) noexcept;
+			friend int DirectoryListing(DirFileList& output, const char* path);
 		};
 
 		int MakeDirectory(const char* path, bool recursive = true);
-		int DirectoryListing(DirFileList& output, const char* path) noexcept;
-		int CheckIfDir(const char* path) noexcept;
+		int RemoveDirectory(const char* path, bool recursive = true);
+		int DirectoryListing(DirFileList& output, const char* path);
+		int CheckIfDir(const char* path);
+		int FixUpPath(char*& out, const char* path, bool ending_delimitor = true);
 	}
 }
 
